fc92c311308cae5319e8e7d1a1d47d60b45da62b
ethan dibble <edibble@pdx.edu>
2025-06-29 19:33:47 -0700

    library doc

--- oxid8-kitty/src/lib.rs
+++ oxid8-kitty/src/lib.rs
@@ -1,8 +1,2 @@
-//! # Oxid-8
-//!
-//! `oxid8` is an interpreter core for the Chip-8 programming language,
-//! developed by Joseph Weisbecker in the mid-1970s for making games on the
-//! COSMAC VIP and Telmac 1800.
-
 pub mod app;
 pub mod screens;
--- oxid8/src/lib.rs
+++ oxid8/src/lib.rs
@@ -1,3 +1,9 @@
+//! # Oxid-8
+//!
+//! `oxid8` is an interpreter core for the Chip-8 programming language,
+//! developed by Joseph Weisbecker in the mid-1970s for making games on the
+//! COSMAC VIP and Telmac 1800.
+
 use rand::{Rng, rng, rngs::ThreadRng};
 use std::{fmt, fs, io, time::Duration};
 
